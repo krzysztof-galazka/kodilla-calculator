f2895159b08c418691b8003f41a9fb99d13bf36f
krzysztof-galazka <kgalazka@vp.pl>
2020-03-03 22:34:36 +0100

    Change parameters

--- src/main/java/com/kodilla/kodillacalculator/Calculator.java
+++ src/main/java/com/kodilla/kodillacalculator/Calculator.java
@@ -13,8 +13,8 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.add(5,5);
-        System.out.println("Add: "+calculator.add(5,5));
-        System.out.println("Minus: "+calculator.minus(10,5));
+        System.out.println("Add: "+calculator.add(8,5));
+        System.out.println("Minus: "+calculator.minus(23,5));
     }
 }
 
