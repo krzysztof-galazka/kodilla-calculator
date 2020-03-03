47c39ce00b762d33d3a042a655aca78dee07b521
krzysztof-galazka <kgalazka@vp.pl>
2020-03-03 22:31:42 +0100

    Simple calculator

---
+++ src/main/java/com/kodilla/kodillacalculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.kodilla.kodillacalculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int minus(int a, int b) {
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(5,5);
+        System.out.println("Add: "+calculator.add(5,5));
+        System.out.println("Minus: "+calculator.minus(10,5));
+    }
+}
+
